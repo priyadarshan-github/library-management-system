67a8555ac87a8636d686169b45b4b04e436f139d
priyadarshangarade <garadepriyadarshan@gmail.com>
2025-01-18 18:40:23 +0000

    Initial Commit with project structure

---
+++ src/test/java/com/priyadarshan/library_management_system/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyadarshan.library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
