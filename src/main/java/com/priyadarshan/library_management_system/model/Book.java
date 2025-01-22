9385bb3eaf23d3b066866fb35b0a48bef27ebf7e
priyadarshangarade <garadepriyadarshan@gmail.com>
2025-01-22 12:07:55 +0000

    Initial Commit with project structure

---
+++ src/main/java/com/priyadarshan/library_management_system/model/Book.java
@@ -0,0 +1,10 @@
+package com.priyadarshan.library_management_system.model;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private String isbn;
+    private Date publishedDate;
+
+}
